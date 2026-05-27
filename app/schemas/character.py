bab44da89b4fc940c6cc214a2d6175735f983b97
RattusRex <155375420+RattusRex@users.noreply.github.com>
2026-05-27 21:42:20 +0200

    Enhance CharacterCreate and CharacterUpdate models
    Added additional attributes for character creation and update.

--- app/schemas/character.py
+++ app/schemas/character.py
@@ -6,11 +6,35 @@ class CharacterCreate(BaseModel):
     class_name: str
     level: int
     route: str
+    subclass: str = ""
+    race: str = ""
+    background: str = ""
+    strength: int = 8
+    dexterity: int = 8
+    constitution: int = 8
+    intelligence: int = 8
+    wisdom: int = 8
+    charisma: int = 8
+    investigation: int = 0
+    hp: int = 0
+    armor_class: int = 9
 
 class CharacterUpdate(BaseModel):
     name: Optional[str] = None
     class_name: Optional[str] = None
     xp: Optional[int] = None
     route: Optional[str] = None
+    subclass: Optional[str] = None
+    race: Optional[str] = None
+    background: Optional[str] = None
+    strength: Optional[int] = None
+    dexterity: Optional[int] = None
+    constitution: Optional[int] = None
+    intelligence: Optional[int] = None
+    wisdom: Optional[int] = None
+    charisma: Optional[int] = None
+    investigation: Optional[int] = None
+    hp: Optional[int] = None
+    armor_class: Optional[int] = None
 
 
